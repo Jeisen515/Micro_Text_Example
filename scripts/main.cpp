ec164c970792cfb369a470fd874473526e8555e4
Jeisen515 <jeisen.soler@upr.edu>
2022-08-23 15:35:48 -0400

    This is another commit

---
+++ scripts/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <unordered_set>
+#include <set>
+
+using namespace std;
+
+int main(){
+
+int array[] = {1,2,3,4,4,5,6,7,8,8,9,10};
+int frequency = 0;
+unordered_set<int> set;
+
+for(unsigned int i = 0; i < 12; i++){
+    if(set.find(array[i]) != set.end()){
+        frequency++;
+    } 
+    else{
+        set.insert(array[i]);
+    }
+
+}
+
+
+return frequency;
+}
